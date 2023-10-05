6d640ff606671db4fd58994306a40f988c5b1df2
quan201002 <duongminhquan313@gmail.com>
2023-10-05 20:08:23 +0700

    update

--- bai1.js
+++
@@ -1,8 +0,0 @@
-var array = [3,-1,5,7,-8,10,23,10,11,16];
-var tongduong= 0;
-for(i = 0 ; i < array.length ; i++){
-    if(array[i] > 0){
-        tongduong += array[i];
-    }
-}
-document.getElementById("result").innerText = `tổng các phần tử dương trong mảng: ${tongduong}`;
